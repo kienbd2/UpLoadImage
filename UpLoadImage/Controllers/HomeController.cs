83ae9af846519a659378bd7944cb7b2c5190e58b
ADMIN <ADMIN@DESKTOP-0GHR3KF>
2020-11-15 23:51:08 +0700

    update

--- UpLoadImage/Controllers/HomeController.cs
+++ UpLoadImage/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,28 +17,40 @@ namespace UpLoadImage.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase file)
+        public ActionResult Index(HttpPostedFileBase file2)
         {
-            Account account = new Account(
-           "dev2020",
-           "247996535991499",
-           "9jI_5YjJaseBKUrY929sUtt0Fy0");
-            //MemoryStream ms = new MemoryStream();
-            //ms = new MemoryStream(file.fi)
-            string pic = Path.GetFileName(file.FileName);
-            string path = Path.Combine(Server.MapPath("Images"), Path.GetFileName(file.FileName));
-      
-            
-            Cloudinary cloudinary = new Cloudinary(account);
-            var uploadParams = new ImageUploadParams()
+            if (!ModelState.IsValid)
             {
-                File = new FileDescription(path, file.InputStream),
-            };
-            var uploadResult = cloudinary.Upload(uploadParams);
-             var ruta = uploadResult.SecureUrl.ToString();
-            return View();
+                return Json(new { result = false, mess = "Not valid" });
+            }
+            var lstImage = new List<string>();
+            HttpFileCollectionBase files = Request.Files;
+            if (files.Count > 5)
+            {
+                return Json(new { result = false, mess = "Không up lớn hơn 5 ảnh", url = Url.Action("Index", "Apartment") });
+            }
+
+            Task task = Task.Run(async () =>
+            {
+                for (int i = 0; i < files.Count; i++)
+                {
+                    HttpPostedFileBase file = files[i];
+                    Account account = new Account("dev2020", "247996535991499", "9jI_5YjJaseBKUrY929sUtt0Fy0");
+
+                    string path = Path.Combine(Server.MapPath("Images"), Path.GetFileName(file.FileName));
+                    Cloudinary cloudinary = new Cloudinary(account);
+                    var uploadParams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(path, file.InputStream),
+                    };
+                    var uploadResult = await cloudinary.UploadAsync(uploadParams);
+                    lstImage.Add(uploadResult.SecureUrl.ToString());
+                }
+            });
+            task.Wait();
+            return Json(new { result = true, url = Url.Action("Index", "Apartment") });
         }
-       
+
         public ActionResult About()
         {
 
